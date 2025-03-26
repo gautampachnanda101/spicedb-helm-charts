cae11fc61b5556b8c850783ad1307efcdcb1cda1
Gautam Pachnanda <pachnanda.gautam@gmail.com>
2025-03-26 22:12:30 +0000

    Fix traefik deployment

--- deploy-helm-chart.sh
+++ deploy-helm-chart.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e -x
 function install_mkcert() {
   brew install mkcert
   brew install nss
@@ -6,17 +7,26 @@ function install_mkcert() {
 }
 function install_traefik() {
     helm uninstall traefik -n traefik || true
-    helm repo add traefik https://traefik.github.io/charts
+    if ! helm repo list | grep -q 'https://traefik.github.io/charts'; then
+            helm repo add traefik https://traefik.github.io/charts
+    fi
     helm repo update
+    kubectl delete namespace traefik || true
     kubectl create namespace traefik
+    kubectl -n traefik create secret tls zerossl-ca  \
+    --key "$(mkcert -CAROOT)"/rootCA-key.pem \
+    --cert "$(mkcert -CAROOT)"/rootCA.pem
+    kubectl apply -f ./traefik/helm/templates/traefik-certs.yaml -n traefik
     helm install traefik traefik/traefik --namespace traefik --values ./traefik/helm/values.yaml
     kubectl get pods -n traefik
     kubectl get svc -n traefik
+    kubectl get ingress -n traefik
 }
 function add_cert_manager() {
     helm uninstall cert-manager -n cert-manager || true
     helm repo add jetstack https://charts.jetstack.io
     helm repo update
+    kubectl delete namespace cert-manager || true
     kubectl create namespace cert-manager
     helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v1.10.0 --set installCRDs=true
     kubectl get pods --namespace cert-manager
@@ -32,7 +42,7 @@ function install_spicedb() {
   helm install spicedb ./spicedb/helm --namespace spicedb -f ./spicedb/helm/values.yaml
 }
 function install_cluster_issuer() {
-  kubectl delete secret cluster-ca-secret -n cert-manager
+  kubectl delete secret cluster-ca-secret -n cert-manager || true
   kubectl create secret tls cluster-ca-secret \
   --key "$(mkcert -CAROOT)"/rootCA-key.pem \
   --cert "$(mkcert -CAROOT)"/rootCA.pem -n cert-manager
@@ -59,12 +69,13 @@ function install_otel_lgtm() {
   kubectl create namespace otel-lgtm
   helm install otel-lgtm ./otel-lgtm/helm  --namespace otel-lgtm
 }
-kubectl version | brew install kubectx kubectl
+kubectl version | brew install kubectl
+kubectx -h  | brew install kubectx
 kubectx | kubectx docker-desktop
-install_traefik
-add_cert_manager
 install_mkcert
+add_cert_manager
 install_cluster_issuer
+install_traefik
 install_otel_lgtm
 install_spicedb_operator
 install_spicedb_postgres
